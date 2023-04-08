5e4a10e8ab5f4ff66525c161f5e324d6fc6ed352
zhang564-code <564283743@qq.com>
2023-04-08 15:05:48 +0800

    feat(custom): new commitizen

--- src/.cz-config.js
+++ src/.cz-config.js
@@ -24,6 +24,8 @@ module.exports = {
     footer: '请输入要关闭的issue(可选):',
     confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
   },
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix'],
   // 跳过问题
   skipQuestions: ['body', 'footer'],
   // subject文字长度默认是72

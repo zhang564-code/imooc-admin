1753e9c8f0c681f187371a46b19f6a02d106da57
zhang564-code <564283743@qq.com>
2023-04-09 18:01:09 +0800

    feat(custom): 简易的登录表单校验
    1.为el-form绑定model属性 2.为el-form绑定rules属性 3.为el-form-item绑定prop属性

--- src/views/login/login_index.vue
+++ src/views/login/login_index.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="login-container">
-    <el-form class="login-form">
+    <el-form class="login-form" :model="loginForm" :rules="loginRules">
       <div class="title-container">
         <h3 class="title">用户登录</h3>
       </div>
       <!-- username -->
-      <el-form-item>
+      <el-form-item prop="username">
         <span class="svg-container">
           <svgIcon icon="user"></svgIcon>
         </span>
-        <el-input placeholder="username" name="username" type="text"></el-input>
+        <el-input
+          placeholder="username"
+          name="username"
+          type="text"
+          v-model="loginForm.username"
+        ></el-input>
       </el-form-item>
       <!-- password -->
-      <el-form-item>
+      <el-form-item prop="password">
         <span class="svg-container">
           <svgIcon icon="password"></svgIcon>
         </span>
-        <el-input placeholder="password" name="password"></el-input>
+        <el-input
+          placeholder="password"
+          name="password"
+          v-model="loginForm.password"
+        ></el-input>
         <span class="show-pwd">
           <span class="svg-container">
             <svgIcon icon="eye"></svgIcon>
@@ -32,8 +41,31 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import svgIcon from '@/components/SvgIcon/SvgIcon_index.vue'
-import {} from 'vue'
+import { validatePassword } from './rules'
+//  数据源
+const loginForm = ref({
+  username: 'admin',
+  password: '123456'
+})
+//  验证规则
+const loginRules = ref({
+  username: [
+    {
+      required: true,
+      trigger: 'blur',
+      message: '用户名不能为空'
+    }
+  ],
+  password: [
+    {
+      required: true,
+      trigger: 'blur',
+      validator: validatePassword()
+    }
+  ]
+})
 </script>
 
 <style lang="scss" scoped>

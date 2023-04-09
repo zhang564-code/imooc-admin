5edda67fb828b6cb903af7a392b93fc1ad6414ae
zhang564-code <564283743@qq.com>
2023-04-09 10:10:10 +0800

    feat(custom): 构建登录页面UI结构
    简单的form表单使用
    
    ISSUES CLOSED: 暂无

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const routes = []
+/**
+ * 公开路由表
+ */
+const publicRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/login_index')
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes: publicRoutes
 })
 
 export default router

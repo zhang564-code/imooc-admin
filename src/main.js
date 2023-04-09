7abb52bb53691d2cfea2c0826e949552697a1825
zhang564-code <564283743@qq.com>
2023-04-09 11:27:33 +0800

    feat(custom): 美化登录页面样式login_index.vue中的css样式
    简单的form表单使用
    ISSUES CLOSED: 暂无

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
+//  导入index.scss 初始化样式表
+import '@/styles/index.scss'
 
 const app = createApp(App)
 installElementPlus(app)

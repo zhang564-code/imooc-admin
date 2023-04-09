94217870a5a82581b9a7463157bc4199d10c60c0
zhang564-code <564283743@qq.com>
2023-04-09 08:45:26 +0800

    refactor(custom): 配置代码格式规范 重构代码结构 导入element-plus
    增加第一章commitizen commitlint配置 删除Home、HelloWorld、About等无用页面 修改路由
    
    ISSUES CLOSED: 暂无

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,26 +2,26 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   roles: {
     'type-enum': [
-      //当前验证的错误级别
+      // 当前验证的错误级别
       2,
-      //在什么情况下进行验证
+      // 在什么情况下进行验证
       'always',
-      //泛型的内容具体是什么
+      // 泛型的内容具体是什么
       [
-        'feat',//增加新功能
-        'fix',//修复 bug
-        'docs',//文档注释
-        'style',//代码格式
-        'refactor',//重构
-        'perf',//性能优化
-        'test',//新增测试
-        'chore',//构建过程或辅助工具的变动
-        'revert',//回退
-        'build'//打包
+        'feat', // 增加新功能
+        'fix', // 修复 bug
+        'docs', // 文档注释
+        'style', // 代码格式
+        'refactor', // 重构
+        'perf', // 性能优化
+        'test', // 新增测试
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回退
+        'build'// 打包
       ]
 
     ],
-    //大小写不作校验
+    // 大小写不作校验
     'subject-case': [0]
   }
 }
